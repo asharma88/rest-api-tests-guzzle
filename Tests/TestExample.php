8e2dc7b048af975e5432612d2ed4f2a1dc63b0f9
Arjun Sharma <arjunsharma@arjuns-mbp.home>
2017-08-30 21:56:32 +0100

    adding some basic assertions

--- Tests/TestExample.php
+++ Tests/TestExample.php
@@ -3,9 +3,28 @@
 class TestExample extends Tests\TestBase {
 
   public function testBasicGet() {
+      $expected_body = "quiaetsuscipitsuscipitrecusandaeconsequunturexpeditae"
+                      ."tcumreprehenderitmolestiaeututquastotamnostrumrerumes"
+                      ."tautemsuntremevenietarchitecto";
+
+      // url request -> get
       $url = sprintf('https://%s/posts/1', self::$api_host);
       $response = self::$client->request('GET', $url, ['http_errors' => false]);
+      // check http status code
       $this->assertEquals(200, $response->getStatusCode());
+      // decode json object returned from request
+      $body = json_decode($response->getBody(), true);
+      // make assertions to verify response
+      $this->assertEquals(1, $body['userId']);
+      $this->assertEquals(1, $body['id']);
+      $this->assertEquals(
+        "sunt aut facere repellat provident occaecati "
+        ."excepturi optio reprehenderit",
+        $body['title']
+      );
+
+      $actual_body = preg_replace('/\s+/', '', $body['body']);
+      $this->assertEquals($expected_body, $actual_body);
   }
 
 }
